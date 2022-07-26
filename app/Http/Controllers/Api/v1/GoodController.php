cc6d57b796faadff568cf76fd95bde08966575d6
hjkim <hjkim3351@agentsoft.co.kr>
2022-07-26 16:53:11 +0900

    update

--- app/Http/Controllers/Api/v1/GoodController.php
+++ app/Http/Controllers/Api/v1/GoodController.php
@@ -8,10 +8,18 @@ use App\Http\Resources\v1\GoodResource;
 use App\Http\Resources\v1\GoodCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-
+/**
+ * 상품 API
+ */
 class GoodController extends Controller
 {
 
+    /**
+     * 상품 조회([GET]/api/v1/good)
+     *
+     * @param request $request 상품정보
+     * @return json
+     */
     public function index(Request $request)
     {
 
@@ -25,10 +33,12 @@ class GoodController extends Controller
         if($request->get('goods_nm')){
             $goods=$goods->where('goods_nm', 'LIKE', '%'.$request->get('goods_nm').'%');
         }
+
         //업체코드
         if($request->get('com_id')){
             $goods=$goods->where('com_id',$request->get('com_id'));
         }
+
         //상품설명 LIKE 검색
         if($request->get('goods_cont')){
             $goods=$goods->where('goods_cont', 'LIKE', '%'.$request->get('goods_cont').'%');
@@ -39,6 +49,12 @@ class GoodController extends Controller
         return new GoodCollection($goods);
     }
 
+    /**
+     * 상품 개별 상세조회([GET]/api/v1/good/{goods_no})
+     *
+     * @param int $id 상품번호
+     * @return json
+     */
     public function show($id)
     {
         $good = Good::where('goods_no', $id)->firstOrFail();
@@ -46,6 +62,12 @@ class GoodController extends Controller
         return new GoodResource($good);
     }
 
+    /**
+     * 상품 입력([POST]/api/v1/good)
+     *
+     * @param request $request 상품정보
+     * @return json
+     */
     public function store(Request $request)
     {
         //마지막 id값가져 오기
@@ -65,6 +87,13 @@ class GoodController extends Controller
         return new GoodResource($good);
     }
 
+    /**
+     * 상품 수정([PUT]/api/v1/good/{goods_no})
+     *
+     * @param request $request 상품정보
+     * @param int $id 상품번호
+     * @return json
+     */
     public function update(Request $request, $id)
     {
         $data = $request->all();
